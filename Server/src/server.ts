7f3516a3b7f944bf84ccec2e8733562d3bfd557e
pakhoakuy66 <phananhkhoa41004@gmail.com>
2025-07-23 16:13:21 +0700

    Chore: Setup BE and git-Actions

---
+++ Server/src/server.ts
@@ -0,0 +1,12 @@
+import express, { Application } from "express";
+
+const app: Application = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World:)) hehe");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
