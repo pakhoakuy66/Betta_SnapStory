3d07a3723158f47c09ffb0682bc0a112c55d9d0d
pakhoakuy66 <phananhkhoa41004@gmail.com>
2025-08-02 00:15:08 +0700

    feat: Design UI Profile of user

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,11 +4,12 @@ import { Register } from "./Pages/Auth/register";
 import { ForgotPassword } from "./Pages/Auth/forgotPassword";
 import { UserHome } from "./Pages/Home/UserHomePage";
 import { HidePost } from "./components/customs/Context_menu/Chores/hidePost";
+import { Profile } from "./Pages/User/UserPage";
 
 function App() {
     return (
         <div className="bg-[#000000] w-full overflow-x-hidden min-h-screen">
-            <UserHome />
+            <Profile />
         </div>
     );
 }
