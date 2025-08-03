34011e3831e9d077f0061dbbf81395321b094e52
pakhoakuy66 <phananhkhoa41004@gmail.com>
2025-08-04 00:03:42 +0700

    feat: Display menu user settings

--- Client/src/components/customs/User/Header/UserHeader.tsx
+++ Client/src/components/customs/User/Header/UserHeader.tsx
@@ -1,8 +1,29 @@
+import { useState, useEffect, useRef } from "react";
+import { UserSettings } from "../Context_Menu/UserSettings";
+
 export function UserHead() {
+    const [showMenuSettings, setShowMenuSettings] = useState(false);
+
+    const menuSettingsRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutSide = (e: MouseEvent) => {
+            if (
+                menuSettingsRef.current &&
+                !menuSettingsRef.current.contains(e.target as Node)
+            ) {
+                setShowMenuSettings(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutSide);
+        return () =>
+            document.removeEventListener("mousedown", handleClickOutSide);
+    }, []);
+
     return (
-        <header className="flex items-center gap-10 p-8 bg-amber-500">
+        <header className="flex items-center gap-10 p-8">
             {/* Avatar */}
-            <div className="flex justify-center items-center w-[200px] h-[200px] bg-amber-800 rounded-full overflow-hidden">
+            <div className="flex justify-center items-center w-[200px] h-[200px] border-1 rounded-full overflow-hidden">
                 <img
                     src="https://via.placeholder.com/150"
                     alt="Avatar"
@@ -11,17 +32,43 @@ export function UserHead() {
             </div>
 
             {/* Thông tin bên phải */}
-            <div className="flex-1 grid bg-amber-950 rounded-md p-6">
+            <div className="flex-1 grid rounded-md p-6">
                 <div className="flex items-center justify-between mb-[10px]">
                     <div className="text-[#C7D5E0] text-[26px] font-bold">
                         Tên người dùng
                     </div>
-                    <button className="cursor-pointer">
+                    <div
+                        ref={menuSettingsRef}
+                        className="cursor-pointer relative"
+                    >
                         <i
+                            onClick={() => setShowMenuSettings((prev) => !prev)}
                             className="fa-solid fa-gear
                                     text-[20px] hover:text-[25px] duration-500 hover:drop-shadow-[0_0_10px_white]"
                         ></i>
-                    </button>
+                        {showMenuSettings && (
+                            <UserSettings
+                                options={[
+                                    {
+                                        label: "Ngôn ngữ",
+                                        action: "Languages",
+                                    },
+
+                                    { label: "Chia sẻ QR", action: "QRCode" },
+                                    {
+                                        label: "Cài đặt & quyền riêng tư",
+                                        action: "QRCode",
+                                    },
+                                    {
+                                        label: "Hoạt động đăng nhập",
+                                        action: "Login_Activity",
+                                    },
+                                    { label: "Đăng xuất", action: "Logout" },
+                                ]}
+                                onOptionClick={(action) => {}}
+                            />
+                        )}
+                    </div>
                 </div>
                 <div className="flex items-center justify-between mb-[10px]">
                     <p className="text-[#C7D5E0] text-[16px]">
@@ -41,13 +88,13 @@ export function UserHead() {
                 </div>
                 <div className="flex items-center my-[10px]">
                     <button
-                        className="w-[180px] h-[30px] bg-[#151d2a] text-white 
+                        className="w-[220px] h-[30px] bg-[#151d2a] text-white 
                                 rounded-sm drop-shadow-[0_0_1px_white] cursor-pointer
                                 duration-300 hover:drop-shadow-[0_0_3px_white] 
                                 active:scale-95 active:drop-shadow-[0_0_5px_white]"
                     >
                         <i className="fa-solid fa-pen-to-square"></i> Chỉnh sửa
-                        bài viết
+                        trang cá nhân
                     </button>
                 </div>
             </div>
